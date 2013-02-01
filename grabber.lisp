99039d666a300568c03b7216157ff3416dc72ace
Rigidus <avenger-f@yandex.ru>
2013-02-01 06:04:59 +0400

    condition/restarts

--- grabber.lisp
+++ grabber.lisp
@@ -9,7 +9,13 @@
 (require 'babel)
 (load #P"puri-unicode.lisp")
 
+
 (defparameter *user-agent* "Mozilla/5.0 (X11; U; Linux i686; ru; rv:1.9.2.13) Gecko/20101206 Ubuntu/10.04 (lucid) Firefox/3.6.13")
+(defparameter *strategy* nil)
+
+(defconstant +many-files+ 3)
+(define-condition to-many-files-error (error)
+  ((text :initarg :text :reader text)))
 
 (define-condition non-200-status-error (error)
     ((text :initarg :text :reader text)))
@@ -17,12 +23,31 @@
 (define-condition non-download-link-error (error)
   ((text :initarg :text :reader text)))
 
+(define-condition file-exists-error (error)
+  ((text :initarg :text :reader text))
+  (:report (lambda (condition stream)
+             (format stream "File '~A' exists!" (text condition)))))
+
+
 
 (defmacro bprint (var)
   `(subseq (with-output-to-string (*standard-output*)  (pprint ,var)) 1))
 
 (defmacro err (var)
   `(error (format nil "ERR:[~A]" (bprint ,var))))
+
+(defmacro make-sanitize (deprecated)
+  `(defun sanitize (string)
+     (let ((ret))
+       (loop :for x :across string :do
+          (or
+           ,@(loop :for character :across deprecated :collect
+                `(equal x ,character))
+           (push x ret)))
+       (coerce (reverse ret) 'string))))
+
+(make-sanitize "()[]{}'\"!%\/")
+
 
 
 (defun get-link (body)
@@ -38,30 +63,7 @@
         (error 'non-200-status-error :text (format nil "~A : ~A" number status-code))
         (funcall fun body-or-stream))))
 
-(defun sanitize ($string)
-  (cond ((vectorp $string) (let (($ret nil))
-							 (loop
-								for x across $string collect x
-								do (if (not
-										(or
-                                         (equal x #\()
-                                         (equal x #\))
-                                         (equal x #\[)
-                                         (equal x #\])
-                                         (equal x #\{)
-                                         (equal x #\})
-										 (equal x #\')
-										 (equal x #\")
-										 (equal x #\!)
-										 (equal x #\%)
-										 (equal x #\\)
-										 (equal x #\/)
-										 ))
-									   (push x $ret)))
-							 (coerce (reverse $ret) 'string)))
-		((listp $string)   (if (null $string)
-							   ""
-							   $string))))
+
 
 (defun download (number)
   (let* ((link  (get-page number #'get-link))
@@ -72,30 +74,86 @@
        fname
        body-or-stream))))
 
+(defun read-new-fname ()
+  (format t "Enter a new file name (in quotes): ")
+  (list (safe-fname (car (multiple-value-list (eval (read)))))))
+
+(defun safe-fname(fname)
+  (flet ((find-rename-num (fname)
+           (do ((i 0))
+               ((> i +many-files+) (error 'to-many-files-error))
+             (incf i)
+             (if (not (probe-file (pathname (format nil "./files/~A(~A)" fname i))))
+                 (let ((old-fname fname))
+                   (setf fname (format nil "~A(~A)" fname i))
+                   (format t "Warn: Existing file '~A' renamed to ~A~%" old-fname fname)
+                   (return-from find-rename-num fname))))))
+    (restart-case
+        (if (probe-file (pathname (format nil "./files/~A" fname)))
+            (error 'file-exists-error :text fname))
+      (overwrite ()
+        :report "Overwrite the file. Old file will be lost."
+        (format t "Warn: Existing file '~A' overwrited~%" fname)
+        fname)
+      (overwrite-all ()
+        :report "Overwrite all files."
+        (progn
+          (setf *strategy* 'overwrite)
+          (safe-fname fname)))
+      (skip ()
+        :report "Skip to a new file. The old file will be saved."
+        (progn
+          (format t "Warn: Existing file '~A' skipped~%" fname)
+          (return-from safe-fname fname)))
+    (skip-all ()
+      :report "Skip all new file."
+      (progn
+        (setf *strategy* 'skip)
+        (safe-fname fname)
+        (return-from safe-fname fname)))
+    (enter-name (new-fname)
+      :report "Enter a name for the new file"
+      :interactive read-new-fname (setf fname new-fname))
+    (auto-rename ()
+      :report "Add sequence number to the file name"
+      (setf fname (find-rename-num fname)))
+    (auto-rename-all ()
+      :report "Add sequence number to all files"
+      (progn
+        (setf *strategy* 'rename)
+        ;; (safe-fname fname)
+        (setf fname (find-rename-num fname)))))
+  fname))
+
 (defun save (number)
   (multiple-value-bind (fname fdata)
       (download number)
+    (setf fname (safe-fname fname))
     (format t "~A: ~A~%" number fname)
     (let ((stream-type (cadr (type-of fdata))))
       (with-open-file (stream (pathname (format nil "./files/~A" fname))
                               :element-type stream-type
                               :direction :output
                               :if-exists :supersede)
-            (write-sequence fdata stream)))))
+        (format t "Write to '~A' ~%" fname)
+        (write-sequence fdata stream)))))
 
-;; (multiple-value-bind (fname fdata)
-;;     (download 43450113)
-;;   (cadr (type-of fdata)))
 
 (defun rghost(start end)
   (format t "=RGHOST=~%")
   (do ((i start))
       ((< i end))
-    (handler-case
-        (save i)
-      (non-200-status-error (se)    (format t "~A: ~A~%" (bprint se) (text se)))
-      (non-download-link-error (se) (format t "~A: ~A: ~A~%" (bprint se) i (text se))))
+    (handler-bind ((file-exists-error
+                    #'(lambda (se)
+                        (cond ((equal *strategy* 'skip)       (invoke-restart 'skip))
+                              ((equal *strategy* 'overwrite)  (invoke-restart 'overwrite))
+                              ((equal *strategy* 'rename)     (invoke-restart 'auto-rename))
+                              (t nil)))))
+      (handler-case
+          (save i)
+        (non-200-status-error (se)    (format t "~A: ~A~%" (bprint se) (text se)))
+        (non-download-link-error (se) (format t "~A: ~A: ~A~%" (bprint se) i (text se)))))
     (decf i))
   (print 'fin))
 
-(rghost 43450111 43450100)
+(rghost 43450110 43450107)
